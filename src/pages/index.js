1fe6bc53557dd5a86ae392c3189dc791b37ab94c
cwithall <chriswithall@MacBook-Pro-8.local>
2019-01-18 14:20:09 +0000

    Added node map

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,51 @@ import React, { Fragment } from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
 
 
-const HomePage = ({ data }) => {
-  return <div>{data.contentfulAuthor.name}</div>
+export default ({ data }) => {
+  return (
+    <div>
+      <h1>Index page</h1>
+      {data.allContentfulBlogPost.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.title} <span>— {node.date}</span>
+          </h3>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export const query = graphql`
-         query authourName {
-           contentfulAuthor {
-             name
-           }
-         }
-       `
+query Posts{
+  allContentfulBlogPost{
+    edges{
+      node{
+        title
+        date
+        content{
+          content
+        }
+        image{
+          fixed{
+             base64
+       tracedSVG
+       aspectRatio
+       width
+       height
+       src
+       srcSet
+       srcWebp
+       srcSetWebp
+          }
+        }
+        authorName{
+          name
+        }
+        
+        
+      }
+    }
+  }
+}`
 
-export default HomePage
