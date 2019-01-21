2078d6a75a83c9fdc3cf539a0e5ccd10d9f2c5eb
avi312singh <avi312singh@gmail.com>
2019-01-21 15:54:12 +0000

    Working site :)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,59 @@
-import React, { Fragment } from 'react'
-import Content from './content'
+import React from 'react'
+import Img from 'gatsby-image'
+import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
-class index extends React.Component{
-  render(){
-    return <Fragment>
-        return content ffs:
-        <Content>
 
-        </Content>
-      </Fragment>
-  }
+export default ({ data }) => {
+ return <Layout>
+     <div>
+       <h1>Test Blog</h1>
+       {data.allContentfulBlogPost.edges.map(({ node }) => <div key={node.id}>
+           <h3>{node.title}</h3>
+           <small>Created on {node.date}</small>
+           <br />
+           <Img fixed={node.image.fixed} />
+           <br />
+           <br />
+           <br />
+           <h3>{node.content.content}</h3>
+           <br />
+           <br />
+           <small>Created by {node.authorName[0].name}</small>
+         </div>)}
+     </div>
+   </Layout>
 }
 
+export const query = graphql`
+query Posts{
+ allContentfulBlogPost{
+   edges{
+     node{
+       title
+       date(formatString: "Do MMMM YYYY")
+       content{
+         content
+       }
+       image{
+         fixed{
+            base64
+      tracedSVG
+      aspectRatio
+      width
+      height
+      src
+      srcSet
+      srcWebp
+      srcSetWebp
+         }
+       }
+       authorName{
+         name
+       }
 
-export default index
+
+     }
+   }
+ }
+}`
