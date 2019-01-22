5035cbae4fa6ea6833299bd0f204f5c22f6193d2
cwithall <chriswithall@MacBook-Pro-8.local>
2019-01-22 13:35:16 +0000

    fixed content component

---
+++ src/components/content.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Img from 'gatsby-image';
+import {StaticQuery, graphql} from'gatsby';
+
+export default () => (
+   <StaticQuery
+       query={graphql`
+       query BlogPosts{
+         allContentfulBlogPost{
+           edges{
+             node{
+               title
+               date(formatString: "Do MMMM YYYY")
+               content{
+                 content
+               }
+               image{
+                 fixed{
+                    base64
+              tracedSVG
+              aspectRatio
+              width
+              height
+              src
+              srcSet
+              srcWebp
+              srcSetWebp
+                 }
+               }
+               authorName{
+                 name
+               }
+
+
+             }
+           }
+         }
+       }`
+
+       }
+       render={data => (
+           <div>
+    <h1>Test Blog</h1>
+    {data.allContentfulBlogPost.edges.map(({ node }) => (
+      <div key={node.id}>
+        <h3>
+          {node.title}
+        </h3>
+        <small>Created on {node.date}</small><br/>
+        <Img fixed={node.image.fixed}/><br/>
+        <br/>
+        <br/>
+        <h3>{node.content.content}</h3>
+        <br/>
+        <br/>
+        <small>Created by {node.authorName[0].name}</small>
+      </div>
+    ))}
+  </div>
+
+       )
+
+       }
+   />
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,62 +1,14 @@
 import React, { Fragment } from 'react'
 import Img from 'gatsby-image'
 import { Link, StaticQuery, graphql } from 'gatsby'
-import Blogcontent from '../components/blogcontent'
+import Content from '../components/content'
 
 
-export default ({ data }) => {
-  return (
-    <div>
-      <h1>Test Blog</h1>
-      {data.allContentfulBlogPost.edges.map(({ node }) => (
-        <div key={node.id}>
-          <h3>
-            {node.title} 
-          </h3>
-          <small>Created on {node.date}</small><br/>
-          <Img fixed={node.image.fixed}/><br/>
-          <br/>
-          <br/>
-          <h3>{node.content.content}</h3>
-          <br/>
-          <br/>
-          <small>Created by {node.authorName[0].name}</small>
-        </div>
-      ))}
-    </div>
-  )
-}
+export default () => (
+  <>
+    <Content/>
+  </>
+);
 
-export const query = graphql`
-query Posts{
-  allContentfulBlogPost{
-    edges{
-      node{
-        title
-        date(formatString: "Do MMMM YYYY")
-        content{
-          content
-        }
-        image{
-          fixed{
-             base64
-       tracedSVG
-       aspectRatio
-       width
-       height
-       src
-       srcSet
-       srcWebp
-       srcSetWebp
-          }
-        }
-        authorName{
-          name
-        }
-        
-        
-      }
-    }
-  }
-}`
+
 
