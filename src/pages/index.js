898b63d7a66f3544a644fd8f3f94f90f82852699
avi312singh <avi312singh@gmail.com>
2019-01-22 13:06:20 +0000

    chris's changes and not a blank app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,10 @@
 import React from 'react'
-import Img from 'gatsby-image'
-import Layout from '../components/layout'
-import { graphql } from 'gatsby'
+import content from '../components/content'
 
 
-export default ({ data }) => {
- return <Layout>
-     <div>
-       {data.allContentfulBlogPost.edges.map(({ node }) => <div key={node.id}>
-           <h3>{node.title}</h3>
-           <small>Created on {node.date}</small>
-           <br />
-           <Img fixed={node.image.fixed} />
-           <br />
-           <br />
-           <br />
-           <h3>{node.content.content}</h3>
-           <br />
-           <br />
-           <small>Created by {node.authorName[0].name}</small>
-         </div>)}
-     </div>
-   </Layout>
+export default () => {
+
+<content></content>
+
 }
 
-export const query = graphql`
-query Posts{
- allContentfulBlogPost{
-   edges{
-     node{
-       title
-       date(formatString: "Do MMMM YYYY")
-       content{
-         content
-       }
-       image{
-         fixed{
-            base64
-      tracedSVG
-      aspectRatio
-      width
-      height
-      src
-      srcSet
-      srcWebp
-      srcSetWebp
-         }
-       }
-       authorName{
-         name
-       }
-
-
-     }
-   }
- }
-}`
