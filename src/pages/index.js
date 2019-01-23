c31a1fcd493d6b70efd919f3cd3885d2cfc0c76d
cwithall <chriswithall@MacBook-Pro-8.local>
2019-01-23 14:27:36 +0000

    carousel added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,26 @@ import React from 'react'
 import Content from '../components/content'
 import Footer from '../components/footer'
 import Header from '../components/header'
+import Carousel from '../components/carousel'
 
+const containerStyle = {
+  width: '1000px'
+};
+
+const centerStyle = {
+  display: 'flex', 
+  justifyContent: 'center'
+};
 
 export default () => (
-  <>
+  <div>
     <Header></Header>
+    <Carousel/>
+    <div style={containerStyle}>
     <Content/>
+    </div>
     <Footer/>
-  </>
+    </div>
 );
 
 
