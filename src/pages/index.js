db70a6f167e7440feeb67fade8c31ed6c3224f3f
avi312singh <avi312singh@gmail.com>
2019-01-24 09:36:00 +0000

    fixed carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,26 @@ import React from 'react'
 import Content from '../components/content'
 import Footer from '../components/footer'
 import Header from '../components/header'
+import Carousel from '../components/carousel'
 
+const containerStyle = {
+  width: '1000px'
+};
+
+const centerStyle = {
+  display: 'flex',
+  justifyContent: 'center'
+};
 
 export default () => (
-  <>
-    <Header headerText = "Home Pages"></Header>
-    <Content/>
-    <Footer/>
-  </>
+  <div>
+    <Header></Header>
+    <Carousel />
+    <div style={containerStyle}>
+      <Content />
+    </div>
+    <Footer />
+  </div>
 );
-
 
 
