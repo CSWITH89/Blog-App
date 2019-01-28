271641bd30bc643aef4cc0dc4ab0bb72d9d15c45
avi312singh <avi312singh@gmail.com>
2019-01-28 16:10:57 +0000

    Fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Carousel from '../components/carousel'
 import Abovefooter from '../components/abovefooter';
 import Dropdown from '../components/dropdown';
 import Sidebar from '../components/sidebar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import {Alert, DropdownButton} from  'react-bootstrap';
@@ -30,19 +30,6 @@ export default () => (
   <div>
     <Header></Header>
     <Carousel />
-
-    <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown button
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
-</div>
-
-    <br/>
     <Sidebar/>
     <div style={containerStyle}>
     <Dropdown></Dropdown>
