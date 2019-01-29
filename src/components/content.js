406945e18364bacb0b8a5bf281d628208404bdee
avi312singh <avi312singh@gmail.com>
2019-01-29 15:08:43 +0000

    Minor fix and commented out previewcontent for now

--- src/components/content.js
+++ src/components/content.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Img from 'gatsby-image';
 import {StaticQuery, graphql, Link} from'gatsby';
-import styled from 'styled-components';
+//import styled from 'styled-components';
 import { Button } from 'styled-button-component/lib/components/Button';
 
 const containerStyle = {
@@ -17,19 +17,19 @@ export default props => (
   <StaticQuery
     query={graphql`
       query BlogPosts {
-        props: allContentfulBlogPost(
+        filteredByNewest: allContentfulBlogPost(
           sort: { fields: [createdAt], order: DESC }
         ) {
           edges {
             node {
-                previewContent{
-                    previewContent
-                  }
               createdAt
               title
               date(formatString: "Do MMMM YYYY")
               content {
                 content
+              }
+              previewContent {
+                previewContent
               }
               image {
                 fixed(height: 200) {
@@ -88,7 +88,7 @@ export default props => (
     render={data => (
       <div>
         {console.log(data[props])}
-        {console.log(data.filteredByOldest)}
+        {console.log(data.filteredByNewest)}
         {data.filteredByOldest.edges.map(({ node }) => (
           <div key={node.id}>
             <Link style={linkStyle} to={node.slug}>
@@ -98,22 +98,24 @@ export default props => (
                 <br />
                 <h2>{node.title}</h2>
                 <small>
-                Created by {node.authorName[0].name} on {node.date}
-              </small>
+                  Created by {node.authorName[0].name} on {node.date}
+                </small>
                 <Img style={containerStyle} fixed={node.image.fixed} />
               </div>
             </Link>
             <div>
               <br />
               <br />
-              {node.previewContent.previewContent}
+              {/* {node.previewContent.previewContent} */}
+              {node.content.content}
               <br />
               <br />
-              <Button>Read More</Button>
+              <Link to={node.slug}>
+                <Button>Read More</Button>
+              </Link>
               <br />
               <br />
-              <hr></hr>
-              
+              <hr />
             </div>
           </div>
         ))}
